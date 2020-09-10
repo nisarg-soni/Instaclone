6702089f1509719f56364889d7760e369db8601e
Nisarg Soni <soninisarg98@gmail.com>
2020-09-10 20:51:15 +0530

    added image storage functionality

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+    validates :image, presence: true
+
+  has_attached_file :image, styles: { :square => "500x500!" }
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
